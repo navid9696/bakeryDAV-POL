443961363a44e9620b4576acfdf46711aff6059d
Dawid <dawidbrzeczek18@gmail.com>
2023-09-22 19:27:12 +0200

    upgraded nav section

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 const navBtn = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav')
-
+const overlay = document.querySelector('.overlay')
+const logo = document.querySelector('.logo')
+const blockScroll = document.body
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	nav.classList.toggle('nav--active')
+	overlay.classList.toggle('active')
+	blockScroll.classList.toggle('block-scroll')
+	logo.classList.toggle('fade')
 }
 
 navBtn.addEventListener('click', handleNav)
-
-
-
