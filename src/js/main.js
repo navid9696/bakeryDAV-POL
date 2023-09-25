bb2adbb4b9b32ae277d989ebd3594ff6544be9d0
Dawid <dawidbrzeczek18@gmail.com>
2023-09-25 17:54:38 +0200

    added animated opacity,hover animation and more.Started next section(header)

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,41 @@
 const navBtn = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav')
 const overlay = document.querySelector('.overlay')
-const logo = document.querySelector('.logo')
+const links = document.querySelectorAll('.nav__item')
 const blockScroll = document.body
+
+const overlayVisibility = (overlay.style.visibility = 'hidden')
+
+const closeOverlay = () => {
+	handleOverlay()
+	navBtn.classList.remove('is-active')
+	nav.classList.remove('nav--active')
+	blockScroll.classList.remove('block-scroll')
+}
+
+const handleOverlay = () => {
+	if (overlay.style.visibility == 'hidden') {
+		overlay.style.visibility = 'visible'
+		setTimeout(function () {
+			overlay.classList.add('active')
+		}, 1)
+	} else {
+		setTimeout(function () {
+			overlay.style.visibility = 'hidden'
+		}, 300)
+		overlay.classList.remove('active')
+	}
+}
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	nav.classList.toggle('nav--active')
-	overlay.classList.toggle('active')
+	handleOverlay()
 	blockScroll.classList.toggle('block-scroll')
-	logo.classList.toggle('fade')
 }
 
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('click', e => (e.target === overlay ? closeOverlay() : false))
+links.forEach(link => {
+	link.addEventListener('click', closeOverlay)
+})
