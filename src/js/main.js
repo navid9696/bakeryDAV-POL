b8769fb8f41e76dfb8ec62b7859d75c1142c607f
Dawid <navid96@interia.pl>
2023-11-06 12:42:57 +0100

    during email validation

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,17 @@ const nameForm = document.querySelector('#name')
 const emailForm = document.querySelector('#email')
 const msgForm = document.querySelector('#msg')
 const btnForm = document.querySelector('.contact-form__input-btn')
+const popup = document.querySelector('.contact-form__popup')
+const popupBtn = document.querySelector('.contact-form__popup-btn')
 
 const overlayVisibility = (overlay.style.visibility = 'hidden')
 
 const validateEmail = email => {
 	const emailReg =
 		/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-	return emailReg.test(email)
+
+		if(emailReg.test(msgForm.value))
+	// return emailReg.test(email)
 }
 
 const handleCurrentYear = () => {
@@ -51,23 +55,35 @@ const handleNav = () => {
 	blockScroll.classList.toggle('block-scroll')
 }
 
-const checkForm = () => {
-	if (nameForm.value !== '' && emailForm.validity.patternMismatch == false && msgForm.value !== '') {
-		contactForm.contact_number.value = Math.floor(Math.random() * 100000)
-		emailjs
-			.sendForm('service_4rabq7e', 'template_6wtj7m3', contactForm)
-			.then(() => {
-				console.log('SUCCESS!')
-			})
-			.catch(error => {
-				console.log('FAILED...', error)
-			})
-	} else if (emailForm.validity.patternMismatch) {
-		console.log('error')
-		// nameForm.setAttribute('required', '')
-		// emailForm.setAttribute('required', '')
-		// msgForm.setAttribute('required', '')
-	}
+const showError = (input, msg) => {
+	const formBox = input.parentElement
+	const errorMsg = formBox.querySelector('.contact-form__error-text')
+	formBox.classList.add('error')
+	errorMsg.textContent = msg
+}
+
+const removeError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('error')
+}
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError(el, el.placeholder)
+		} else {
+			removeError(el)
+			contactForm.contact_number.value = Math.floor(Math.random() * 100000)
+			emailjs
+				.sendForm('service_4rabq7e', 'template_6wtj7m3', contactForm)
+				.then(() => {
+					console.log('SUCCESS!')
+				})
+				.catch(error => {
+					console.log('FAILED...', error)
+				})
+		}
+	})
 }
 
 ;(function () {
@@ -76,10 +92,9 @@ const checkForm = () => {
 })()
 
 window.onload = () => {
-	contactForm.addEventListener('submit', event => {
-		event.preventDefault()
-
-		checkForm()
+	contactForm.addEventListener('submit', e => {
+		e.preventDefault()
+		checkForm([msgForm, nameForm, emailForm])
 	})
 }
 
